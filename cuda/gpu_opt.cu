0e308b05296b86f8c5658ebfd6ef88d0ec6595fd
rohitm94 <rohit11094@gmail.com>
2019-12-03 23:00:37 -0500

    remove other error

--- cuda/gpu_opt.cu
+++ cuda/gpu_opt.cu
@@ -1,6 +1,7 @@
 #include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <iostream>
 #include "timer.h"
 
 #define BLOCK_SIZE 256
@@ -34,7 +35,7 @@ void bodyForce(float4 *p, float4 *v,float4 *u,float *m, float dt, int n) {
       float invDist3 = invDist * invDist * invDist;
 
       Ax += mg * dx * invDist3;
-      Ay += mg * dy * invDist3; 
+      Ay += mg * dy * invDist3;
       Az += mg * dz * invDist3;
     }
 
@@ -45,35 +46,38 @@ void bodyForce(float4 *p, float4 *v,float4 *u,float *m, float dt, int n) {
 }
 
 int main(const int argc, const char** argv) {
-  
+
   int nBodies = 30000;
   if (argc > 1) nBodies = atoi(argv[1]);
-  
+
   const float dt = 0.01f; // time step
   const int nIters = 10;  // simulation iterations
-  
+
   int bytes = 3*nBodies*sizeof(float4);
   int mass_size = nBodies * sizeof(float);
   float *buf = (float*)malloc(bytes);
-  float *mass = new float[nBodies];
+  float *mass = NULL;
   BodySystem p = { (float4*)buf, ((float4*)buf) + nBodies };
 
   randomizeBodies(buf, 12*nBodies); // Init pos / vel data
 
-  cudaMallocHost((void **)&mass,mass_size);
+  cudaMallocHost((void **)&mass, mass_size);
+
   for(int l = 0;l<nBodies;l++){
     mass[l] = SOLAR_MASS * (rand() / (float)RAND_MAX);
   }
-
+  std::cout<<"test 1"<<std::endl;
+  std::cout<<mass[0]<<std::endl;
   float *d_mass;
-  cudaMalloc((void **)&d_mass , mass_size));
+
+  cudaMalloc((void **)&d_mass, nBodies * sizeof(float));
 
   float *d_buf;
   cudaMalloc(&d_buf, bytes);
   BodySystem d_p = { (float4*)d_buf, ((float4*)d_buf) + nBodies };
 
   int nBlocks = (nBodies + BLOCK_SIZE - 1) / BLOCK_SIZE;
-  double totalTime = 0.0; 
+  double totalTime = 0.0;
 
   for (int iter = 1; iter <= nIters; iter++) {
     StartTimer();
@@ -87,17 +91,18 @@ int main(const int argc, const char** argv) {
       p.pos[i].x += 0.5*(p.newvel[i].x + p.oldvel[i].x)*dt;
       p.pos[i].y += 0.5*(p.newvel[i].y + p.oldvel[i].y)*dt;
       p.pos[i].z += 0.5*(p.newvel[i].z + p.oldvel[i].z)*dt;
+      std::cout<<i<<std::endl;
     }
 
     const double tElapsed = GetTimer() / 1000.0;
     if (iter > 1) { // First iter is warm up
-      totalTime += tElapsed; 
+      totalTime += tElapsed;
     }
 #ifndef SHMOO
     printf("Iteration %d: %.3f seconds\n", iter, tElapsed);
 #endif
   }
-  double avgTime = totalTime / (double)(nIters-1); 
+  double avgTime = totalTime / (double)(nIters-1);
 
   printf("%d Bodies: average %0.3f Billion Interactions / second\n", nBodies, 1e-9 * nBodies * nBodies / avgTime);
 
